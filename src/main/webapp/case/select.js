78c83c2cff6b28c798a547fcec4152029055acf8
wisdomelon <994352474@qq.comm>
2019-06-22 12:16:10 +0800

    v2.5.4 下拉树初始版本

---
+++ src/main/webapp/case/select.js
@@ -0,0 +1,16 @@
+var menuTree = [{
+	"id":"simpleUse",
+	"title": "常规用法",
+	"last": true,
+	"children":[]
+},{
+	"id":"updateTips",
+	"title": "指定显示框默认文字",
+	"last": true,
+	"children":[]
+},{
+	"id":"selectValue",
+	"title": "下拉树的默认显示值",
+	"last": true,
+	"children":[]
+}];
